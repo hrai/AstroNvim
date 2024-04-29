6266a4a4a122508d56a3c2aa89248659acd6439e
Hangjit Rai <hangjit.rai@outlook.com>
2024-04-29 15:15:25 +1000

    Cleanup

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,184 +1,249 @@
-
 local function get_tabnine_build_string()
-    if vim.fn.has("win32") == 1 then
-        -- use special windows path
-        return "pwsh.exe -file .\\install.ps1"
-    else
-        -- unix path
-        return "./install.sh"
-    end
+  if vim.fn.has "win32" == 1 then
+    -- use special windows path
+    return "pwsh.exe -file .\\install.ps1"
+  else
+    -- unix path
+    return "./install.sh"
+  end
 end
 
 return {
-    {
-        "folke/trouble.nvim",
-        cmd = "TroubleToggle",
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "marko-cerovac/material.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function(_, opts)
+      vim.g.material_style = "deep ocean"
+      require("material").setup(opts)
+      vim.cmd "colorscheme material"
+    end,
+  }, --colorscheme
+  { "Pocco81/auto-save.nvim" },
+  { "tpope/vim-abolish" },
+  { "tpope/vim-repeat" },
+  { "tpope/vim-surround", keys = { "c", "d", "y" } },
+  -- { "kshenoy/vim-signature",         keys = { "m" } }, -- mx - Toggle mark 'x' and display it in the leftmost column
+  { "godlygeek/tabular" },
+  { "preservim/vim-markdown", name = "vim-markdown", ft = "md" },
+  { "tpope/vim-markdown", name = "tpope-markdown", ft = "md" },
+  { "tpope/vim-fugitive" },
+  -- { "tpope/vim-rhubarb" },
+  { "christoomey/vim-tmux-navigator" },
+  { "editorconfig/editorconfig-vim" },
+  { "ggandor/lightspeed.nvim" },
+  { "mg979/vim-visual-multi" },
+  { "elzr/vim-json", ft = "json" },
+  { "justinmk/vim-gtfo" }, --Go to Terminal or File manager
+  { "chrisbra/csv.vim", ft = "csv" },
+  { "psliwka/vim-smoothie" }, --Smooth scrolling
+  { "lambdalisue/suda.vim" },
+  { "Almo7aya/openingh.nvim" },
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git permalink" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git permalink" },
     },
-    { "marko-cerovac/material.nvim" ,
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function(_, opts)
-            vim.g.material_style = "deep ocean"
-            require("material").setup(opts)
-            vim.cmd 'colorscheme material'
-        end,
-    }, --colorscheme
-    { "Pocco81/auto-save.nvim" },
-    { "tpope/vim-abolish" },
-    { "tpope/vim-repeat" },
-    { "tpope/vim-surround",            keys = { "c", "d", "y" } },
-    -- { "kshenoy/vim-signature",         keys = { "m" } }, -- mx - Toggle mark 'x' and display it in the leftmost column
-    { "godlygeek/tabular" },
-    { "preservim/vim-markdown",        name = "vim-markdown",   ft = "md" },
-    { "tpope/vim-markdown",            name = "tpope-markdown", ft = "md" },
-    { "tpope/vim-fugitive" },
-    -- { "tpope/vim-rhubarb" },
-    { "christoomey/vim-tmux-navigator" },
-    { "editorconfig/editorconfig-vim" },
-    { "ggandor/lightspeed.nvim" },
-    { "mg979/vim-visual-multi" },
-    { "elzr/vim-json",                 ft = "json" },
-    { "justinmk/vim-gtfo" },    --Go to Terminal or File manager
-    { "chrisbra/csv.vim",              ft = "csv" },
-    { "psliwka/vim-smoothie" }, --Smooth scrolling
-    { "lambdalisue/suda.vim" },
-    { "Almo7aya/openingh.nvim" },
-    {
-        "linrongbin16/gitlinker.nvim",
-        cmd = "GitLink",
-        opts = {},
-        keys = {
-            { "<leader>gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git permalink" },
-            { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git permalink" },
-        },
-        -- config = function()
-        --   vim.cmd([[
-        --     " command! Gblame :GitLink!<cr>
-        --   ]])
-        -- end,
-    },
-    {
-        "panozzaj/vim-autocorrect",
-        config = function()
-            vim.cmd([[
+    -- config = function()
+    --   vim.cmd([[
+    --     " command! Gblame :GitLink!<cr>
+    --   ]])
+    -- end,
+  },
+  {
+    "panozzaj/vim-autocorrect",
+    config = function()
+      vim.cmd [[
         autocmd filetype * call AutoCorrect()
-        ]])
-        end,
+        ]]
+    end,
+  },
+  {
+    "ethanholz/nvim-lastplace", --Return to last edit position when opening files (You want this!)
+    config = function() require("nvim-lastplace").setup {} end,
+  },
+  { "uga-rosa/cmp-dictionary", dependencies = { "hrsh7th/nvim-cmp" } },
+  { "monaqa/dial.nvim" },
+  {
+    "tzachar/cmp-tabnine",
+    after = "nvim-cmp",
+    -- build = function()
+    --     if jit.os == "Linux" then
+    --         print("Executing install.sh...")
+    --         vim.cmd [[execute ":! ~/.local/share/lunarvim/site/pack/lazy/opt/cmp-tabnine/install.sh"]]
+    --     else
+    --         print("Executing install.ps1...")
+    --         EXECUTE BELOW...
+    --         pwsh "$HOME\AppData\Roaming\lunarvim\site\pack\lazy\opt\cmp-tabnine\install.ps1"
+    --         vim.cmd [[execute ":! pwsh -File $HOME\\AppData\\Roaming\\lunarvim\\site\\pack\\packer\\start\\cmp-tabnine\\install.ps1"]]
+    --         -- vim.cmd [[execute ":! pwsh -Command pwd"]]
+    --         print("Executed install.ps1...")
+    --     end
+    -- end,
+    build = get_tabnine_build_string(),
+    dependencies = "hrsh7th/nvim-cmp",
+  },
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = { "comment", "markdown_inline", "regex", "python" },
+    auto_install = true,
+    highlight = {
+      enable = false,
     },
-    {
-        "ethanholz/nvim-lastplace", --Return to last edit position when opening files (You want this!)
-        config = function()
-            require("nvim-lastplace").setup({})
-        end,
+    indent = {
+      enable = false,
     },
-    { "uga-rosa/cmp-dictionary", dependencies = { "hrsh7th/nvim-cmp" } },
-    { "monaqa/dial.nvim" },
-    {
-        "tzachar/cmp-tabnine",
-        after = "nvim-cmp",
-        -- build = function()
-        --     if jit.os == "Linux" then
-        --         print("Executing install.sh...")
-        --         vim.cmd [[execute ":! ~/.local/share/lunarvim/site/pack/lazy/opt/cmp-tabnine/install.sh"]]
-        --     else
-        --         print("Executing install.ps1...")
-        --         EXECUTE BELOW...
-        --         pwsh "$HOME\AppData\Roaming\lunarvim\site\pack\lazy\opt\cmp-tabnine\install.ps1"
-        --         vim.cmd [[execute ":! pwsh -File $HOME\\AppData\\Roaming\\lunarvim\\site\\pack\\packer\\start\\cmp-tabnine\\install.ps1"]]
-        --         -- vim.cmd [[execute ":! pwsh -Command pwd"]]
-        --         print("Executed install.ps1...")
-        --     end
-        -- end,
-        build = get_tabnine_build_string(),
-        dependencies = "hrsh7th/nvim-cmp",
+  },
+  {
+    "lukas-reineke/headlines.nvim", --This plugin adds horizontal highlights for text filetypes, like markdown, orgmode, and neorg
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true, -- or `opts = {}`
+  },
+  {
+    "AckslD/nvim-neoclip.lua", --Clipboard manager neovim plugin with telescope integration
+    dependencies = {
+      { "kkharji/sqlite.lua", module = "sqlite" },
+      { "nvim-telescope/telescope.nvim" },
     },
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {"comment", "markdown_inline", "regex", "python" },
-        auto_install = true,
-        highlight = {
-            enable = false,
-        },
-        indent = {
-            enable = false,
-        },
-    },
-    {
-        "lukas-reineke/headlines.nvim", --This plugin adds horizontal highlights for text filetypes, like markdown, orgmode, and neorg
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        config = true, -- or `opts = {}`
-    },
-    {
-        "AckslD/nvim-neoclip.lua", --Clipboard manager neovim plugin with telescope integration
-        dependencies = {
-            { "kkharji/sqlite.lua",           module = "sqlite" },
-            { "nvim-telescope/telescope.nvim" },
-        },
-        after = "telescope",
-        config = function()
-            require("neoclip").setup({ default_register = { '"', "+", "*" } })
-        end,
-    },
-    {
-        "airblade/vim-rooter",
-        after = "telescope",
-        config = function()
-            vim.cmd([[
+    after = "telescope",
+    config = function() require("neoclip").setup { default_register = { '"', "+", "*" } } end,
+  },
+  {
+    "airblade/vim-rooter",
+    after = "telescope",
+    config = function()
+      vim.cmd [[
               let g:rooter_patterns = ['.git', '.svn', 'package.json', '!node_modules']
-              ]])
-        end,
-    },
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = {
-            filesystem = {
-                filtered_items = {
-	                visible = true,
-                },
-            },
-        }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        after = "nvim-treesitter",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-    },
-    {
-        "kiyoon/treesitter-indent-object.nvim",
-        keys = {
-            {
-                "ai",
-                "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer()<CR>",
-                mode = { "x", "o" },
-                desc = "Select context-aware indent (outer)",
-            },
-            {
-                "aI",
-                "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer(true)<CR>",
-                mode = { "x", "o" },
-                desc = "Select context-aware indent (outer, line-wise)",
-            },
-            {
-                "ii",
-                "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner()<CR>",
-                mode = { "x", "o" },
-                desc = "Select context-aware indent (inner, partial range)",
-            },
-            {
-                "iI",
-                "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner(true)<CR>",
-                mode = { "x", "o" },
-                desc = "Select context-aware indent (inner, entire range)",
-            },
-        },
-    },
-    {
-        "nvimtools/none-ls.nvim",
-        dependencies = {
-          "nvimtools/none-ls-extras.nvim",
+              ]]
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
         },
       },
-    --[[
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
+  {
+    "kiyoon/treesitter-indent-object.nvim",
+    keys = {
+      {
+        "ai",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer()<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer)",
+      },
+      {
+        "aI",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_outer(true)<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer, line-wise)",
+      },
+      {
+        "ii",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner()<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, partial range)",
+      },
+      {
+        "iI",
+        "<Cmd>lua require'treesitter_indent_object.textobj'.select_indent_inner(true)<CR>",
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, entire range)",
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    opts = function(_, opts)
+      local null_ls = require "null-ls"
+
+      -- opts.debug = true
+      opts.sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettierd.with {
+          filetypes = {
+            "css",
+            "scss",
+            "less",
+            "html",
+            "json",
+            "yaml",
+            "markdown",
+            "graphql",
+          },
+        },
+        require "none-ls.diagnostics.eslint_d",
+        null_ls.builtins.completion.spell,
+      }
+      return opts
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      -- customize the dashboard header
+      opts.section.header.val = {
+        " █████  ███████ ████████ ██████   ██████",
+        "██   ██ ██         ██    ██   ██ ██    ██",
+        "███████ ███████    ██    ██████  ██    ██",
+        "██   ██      ██    ██    ██   ██ ██    ██",
+        "██   ██ ███████    ██    ██   ██  ██████",
+        " ",
+        "    ███    ██ ██    ██ ██ ███    ███",
+        "    ████   ██ ██    ██ ██ ████  ████",
+        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+        "    ██   ████   ████   ██ ██      ██",
+      }
+      opts.section.buttons.val = {
+        opts.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+        opts.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+        opts.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+        opts.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+        opts.button("q", "  > Quit NVIM", ":qa<CR>"),
+      }
+      return opts
+    end,
+  },
+
+  --[[
+  {
+    "williamboman/mason-lspconfig.nvim",
+    function(_, opts)
+      opts.ensure_installed = {
+        "powershell_es",
+        "lua_ls",
+        "pyright",
+        "jsonls",
+        "yamlls",
+        "bashls",
+        -- "csharp_ls",
+        "vimls",
+        "tsserver",
+        "graphql",
+      }
+      return opts
+    end,
+  },
     {
     "nvimtools/none-ls.nvim",
     config = function()
@@ -192,7 +257,8 @@ return {
     end,
     requires = { "nvim-lua/plenary.nvim" },
 }
-]]--
+]]
+  --
 }
 
 --[[
@@ -281,4 +347,5 @@ return {
     end,
   },
 }
-]]--
+]]
+--
