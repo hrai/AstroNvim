80dd32505ae95343bce287cbb68f7ad4a11a1c6d
Hangjit Rai <hangjit.rai@outlook.com>
2024-05-02 16:38:56 +1000

    Cleanup

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -218,7 +218,8 @@ return {
         opts.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
         opts.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
         opts.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-        opts.button("c", "  > Config/Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+        opts.button("c", "  > Config/Settings", ":e $MYVIMRC<CR>"),
+        -- opts.button("c", "  > Config/Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
         opts.button("q", "  > Quit NVIM", ":qa<CR>"),
       }
       return opts
