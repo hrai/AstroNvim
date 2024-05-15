531f76122ab5186ba3b7ac3b6e948339344fbfc3
Hangjit Rai <hangjit.rai@outlook.com>
2024-05-15 15:40:23 +1000

    Cleanup

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -235,7 +235,7 @@ return {
       }
       opts.section.buttons.val = {
         opts.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-        opts.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+        opts.button("f", "  > Find file", ":Telescope find_files<CR>"),
         opts.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
         opts.button("c", "  > Config/Settings", ":e $MYVIMRC<CR>"),
         opts.button("p", "  > Plugins Config/Settings", ":e $MYVIMPLUGINS<CR>"),
