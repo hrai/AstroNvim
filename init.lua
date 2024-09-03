b52eaec2947cc7c3ca9a31633654a0d3fb12e0a3
Hangjit Rai <hangjit.rai@outlook.com>
2024-09-03 16:43:56 +1000

    Cleanup

--- init.lua
+++ init.lua
@@ -511,13 +511,13 @@ end
 -- }
 
 -- make sure to run this code before calling setup()
--- refer to the full lists at https://github.com/folke/which-key.nvim/blob/main/lua/which-key/plugins/presets/init.lua
-local presets = require "which-key.plugins.presets"
-presets.operators["v"] = nil
-presets.operators["c"] = nil
-presets.operators["p"] = nil
-presets.operators["y"] = nil
-presets.operators["<"] = nil
+-- -- refer to the full lists at https://github.com/folke/which-key.nvim/blob/main/lua/which-key/plugins/presets/init.lua
+-- local presets = require "which-key.plugins.presets"
+-- presets.operators["v"] = nil
+-- presets.operators["c"] = nil
+-- presets.operators["p"] = nil
+-- presets.operators["y"] = nil
+-- presets.operators["<"] = nil
 
 require("mason-lspconfig").setup {
   ensure_installed = {
