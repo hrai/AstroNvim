c540741391c6406bd1883366abc655938e609f77
Hangjit Rai <hangjit.rai@outlook.com>
2024-09-10 09:49:59 +1000

    Cleanup

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -316,5 +316,15 @@ return {
       vim.keymap.set("n", "<leader>cd", require("telescope").extensions.zoxide.list)
     end,
   },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    init = function() vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }) end,
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  },
   -- { "rebelot/heirline.nvim", enabled = false },
 }
