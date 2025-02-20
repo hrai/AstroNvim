8435e0e4687a7ea746fd95efca0124de4ee63468
Hangjit Rai <hangjit.rai@outlook.com>
2025-02-20 11:09:59 +1100

    Cleanup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
@@ -12,5 +12,15 @@ return {
       "vim",
       -- add more arguments for adding more treesitter parsers
     })
+
+    opts.highlight = {
+      enable = true,
+      -- disable treesitter for large files
+      disable = function(lang, bufnr) --
+        -- Extend this to other languages by adding `lang == "x"` where x is the language
+        return vim.api.nvim_buf_line_count(bufnr) > 10000 and (lang == "cpp" or lang == "c" or lang == "csv")
+      end,
+      additional_vim_regex_highlighting = false,
+    }
   end,
 }
